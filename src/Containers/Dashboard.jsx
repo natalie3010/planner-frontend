c46a029e55eeafa1576403d1ce4ff1021fe8bb6a
IMohamad <ismail.mohamad@capgemini.com>
2022-06-01 16:38:02 +0100

    added input components to the supply page and formatted data for the backend, added routing for the page

--- src/Containers/Dashboard.jsx
+++ src/Containers/Dashboard.jsx
@@ -4,8 +4,9 @@ import { Navigation } from '../Components/Navigation'
 import { Footer } from '../Components/Footer'
 import { BarChart } from '../Components/BarChart'
 import { CG } from 'cap-shared-components'
-
+import { useNavigate } from 'react-router-dom'
 export const Dashboard = () => {
+  const navigate = useNavigate()
   return (
     <Row justify='between'>
       <Col md={12} align='center' justify='center'>
@@ -15,6 +16,9 @@ export const Dashboard = () => {
             Skills Based On Supply and Demand
           </CG.Heading>
           <BarChart />
+          <>
+            <CG.Button text='Add a supply' onClick={() => navigate('/supply')}></CG.Button>
+          </>
         </Col>
         <Footer />
       </Col>
--- src/Containers/SupplyPage.jsx
+++ src/Containers/SupplyPage.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
-
+import { useState } from 'react/cjs/react.development'
 import { Navigation } from '../Components/Navigation'
 import { Footer } from '../Components/Footer'
 import { Col, Row } from 'react-grid-system'
 
 import { CG } from 'cap-shared-components'
 
+import { useNavigate } from 'react-router-dom'
+
 export const SupplyPage = () => {
+  const navigate = useNavigate()
+  const [id, setId] = useState()
+  const [fName, setFName] = useState('')
+  const [lName, setLName] = useState('')
+  const [status, setStatus] = useState('')
+  const [skillId, setSkillId] = useState()
+  const [notes, setNotes] = useState('')
+  const [type, setType] = useState('')
+  const [location, setLocation] = useState('')
+
+  // end point /api/supply
+  const handleSubmit = (e) => {
+    const data = {
+      applicantID: id,
+      applicantFirstName: fName,
+      applicantLastName: lName,
+      applicantStatus: status,
+      skillsID: skillId,
+      notes: notes,
+      applicantType: type,
+      location: location,
+    }
+    console.log(data)
+  }
+
   return (
     <Row justify='between'>
       <Col md={12} align='center' justify='center'>
@@ -14,17 +41,27 @@ export const SupplyPage = () => {
         <div>
           <CG.Heading>Add a new supply</CG.Heading>
           <CG.Container>
-            <CG.Input label={'Name'} />
-            <CG.Input label={'Last name'} />
-            <CG.Input label={'Notes'} />
-            <CG.Input label={'Location'} />
+            <CG.Input
+              label={'ID'}
+              onInput={(e) => setId(e.target.value)}
+              topLabel={false}
+              margin={0.5}
+              width={'small'}
+            />
+            <CG.Input label={'Fist name'} onInput={(e) => setFName(e.target.value)} margin={0.5} />
+            <CG.Input label={'Last name'} onInput={(e) => setLName(e.target.value)} margin={0.5} />
+            <CG.Input label={'Status'} onInput={(e) => setStatus(e.target.value)} margin={0.5} />
+            <CG.Input label={'Skill ID'} onInput={(e) => setSkillId(e.target.value)} margin={0.5} />
+            <CG.Input label={'Notes'} onInput={(e) => setNotes(e.target.value)} margin={0.5} />
+            <CG.Input label={'Location'} onInput={(e) => setLocation(e.target.value)} margin={0.5} />
+            <CG.Input label={'Applicant type'} onInput={(e) => setType(e.target.value)} margin={0.5} />
+            <CG.Button text='submit' onClick={handleSubmit} />
             <CG.Button
-              text='submit'
+              text='cancel'
               onClick={() => {
-                console.log('submitteeeed')
+                navigate('/protectedRoute/dashboard')
               }}
             />
-            <CG.Button text='cancel' />
           </CG.Container>
         </div>
         <Footer />
