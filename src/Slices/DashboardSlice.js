473f8672d8615596a218d2943d1e058d2faffb6b
IMohamad <ismail.mohamad@capgemini.com>
2022-06-22 14:47:36 +0100

    removed console.log

--- src/Slices/DashboardSlice.js
+++ src/Slices/DashboardSlice.js
@@ -4,14 +4,13 @@ export const dashboardSlice = createSlice({
   name: 'dashboard',
   initialState: {
     dashboardData: null,
-    selectedApplicant: 2,
+    selectedApplicant: 53,
   },
   reducers: {
     setupDashboard: (state, action) => {
       state.dashboardData = action.payload
     },
     addSupplyToDashboard: (state, action) => {
-      console.log(action.payload)
       state.dashboardData[action.payload].supply_count += 1
     },
   },
