37adf5071d47e2b66ef8bf0ed9b11b2e4822af13
Yousuf <jalila.yousuf@capgemini.com>
2022-06-29 16:38:24 +0100

    Changes have been made in Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { CG } from 'cap-shared-components'
 
-export const Footer = () => {
-  return <CG.Footer copyright='Capgemini' />
-}
+export const Footer = () => (
+  <CG.Footer>
+    <CG.HomeLink
+      homeLink={{ name: 'workforce Planner', url: '/workforcePlanner' }}
+      homeIcon={{ type: 'LogoAlt3', height: '26px', width: '26px' }}
+    />
+  </CG.Footer>
+)
