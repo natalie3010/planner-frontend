2a3b7f9a0aa85ab3c07ed0979abfa4bf5c4a48c9
Yousuf <jalila.yousuf@capgemini.com>
2022-07-14 16:39:14 +0100

    Fixed Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import { CG } from 'cap-shared-components'
 
 export const Footer = () => {
   return (
-    <CG.Box padding='20px' position='relative' bottom='0' width='100%'>
+    <CG.Box position='absolute' bottom='0' width='100%'>
       <CG.Footer style={{ padding: '20px' }}>
         <CG.HomeLink
           homeLink={{ name: 'workforce Planner', url: '/workforcePlanner' }}
