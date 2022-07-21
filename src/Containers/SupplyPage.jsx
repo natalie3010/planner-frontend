d76de1638e136ff96047e2614f9a1be75680957b
IMohamad <ismail.mohamad@capgemini.com>
2022-07-21 17:28:30 +0100

    renamed react imports location from cjs to react

--- src/Containers/SupplyPage.jsx
+++ src/Containers/SupplyPage.jsx
@@ -1,9 +1,6 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react/cjs/react.development'
+import React, { useEffect, useState } from 'react'
 import { Col } from 'react-grid-system'
-
 import { CG } from 'cap-shared-components'
-
 import { useNavigate } from 'react-router-dom'
 import { addSupply, getSkills } from '../API'
 import { formatSkills, supplyFormFormatter } from '../Data/Format'
