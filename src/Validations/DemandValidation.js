1231a5c0fb184435855a5f8904da7f87f96c3c11
I Mohamad <ismail.mohamad@capgemini.com>
2022-08-16 15:13:54 +0100

    renamed property names to be consistent with API V2

--- src/Validations/DemandValidation.js
+++ src/Validations/DemandValidation.js
@@ -4,20 +4,20 @@ import formValidators from '../../formValidatorsConfig.json'
 const demandInputs = formValidators.demandForm.inputs
 
 export const demandSchema = yup.object().shape({
-  codeRequisition: demandInputs.demandCodeRequisition.validators[0].required && yup.string().required(),
-  startDate:
+  demandCodeRequisition: demandInputs.demandCodeRequisition.validators[0].required && yup.string().required(),
+  demandStartDate:
     demandInputs.demandStartDate.validators[0].required &&
     yup.string().required().matches(new RegExp(demandInputs.demandStartDate.validators[0].pattern)),
-  originatorName: demandInputs.demandOriginator.validators[0].required && yup.string().required(),
-  skillsID: demandInputs.demandSkill.validators[0].required && yup.string().required(),
-  probability: demandInputs.demandProbability.validators[0].required && yup.string().required(),
-  grade: demandInputs.demandGrade.validators[0].required && yup.string().required(),
-  selectedApplicant: demandInputs.demandSelectedApplicant.validators[0].required && yup.string().required(),
-  status: demandInputs.demandStatus.validators[0].required && yup.string().required(),
-  notes: demandInputs.demandNotes.validators[0].required && yup.string().required(),
-  proposedApplicant: demandInputs.demandProposedApplicant.validators[0].required && yup.string().required(),
-  creationDate:
+  demandOriginator: demandInputs.demandOriginator.validators[0].required && yup.string().required(),
+  demandSkills: demandInputs.demandSkills.validators[0].required && yup.string().required(),
+  demandProbability: demandInputs.demandProbability.validators[0].required && yup.string().required(),
+  demandGrade: demandInputs.demandGrade.validators[0].required && yup.string().required(),
+  demandSelectedApplicant: demandInputs.demandSelectedApplicant.validators[0].required && yup.string().required(),
+  demandStatus: demandInputs.demandStatus.validators[0].required && yup.string().required(),
+  demandNotes: demandInputs.demandNotes.validators[0].required && yup.string().required(),
+  demandProposedApplicant: demandInputs.demandProposedApplicant.validators[0].required && yup.string().required(),
+  demandCreationDate:
     demandInputs.demandCreationDate.validators[0].required &&
     yup.string().required().matches(new RegExp(demandInputs.demandCreationDate.validators[0].pattern)),
-  location: demandInputs.demandLocation.validators[0].required && yup.string().required(),
+  demandLocation: demandInputs.demandLocation.validators[0].required && yup.string().required(),
 })
