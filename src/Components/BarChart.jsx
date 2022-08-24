71fd3574ad9e4f8c67e5a9731d530790b515197a
I Mohamad <ismail.mohamad@capgemini.com>
2022-08-24 11:40:28 +0100

    saving

--- src/Components/BarChart.jsx
+++ src/Components/BarChart.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { CG } from 'cap-shared-components'
 import { formatted_data_template, grouped_options } from '../Data/Format'
 
-export const BarChart = ({ chartData, navigateToListPage }) => {
+export const BarChart = ({ chartData, navigateToListPage, allDemand, allSupply, allSkills }) => {
   const formatted_data = structuredClone(formatted_data_template)
 
   const clickedElementPassUp = (element) => {
@@ -19,9 +19,54 @@ export const BarChart = ({ chartData, navigateToListPage }) => {
         formatted_data.datasets[0].data.push(item['supply_count'])
       })
     }
-
     return formatted_data
   }
+
+  const getRequiredDemandStatus = (allDemand) => {
+    const filteredDemands = allDemand.filter((demand) => {
+      /* return (
+        demand.Status === 'Profiles Required' || 'Demand Validation' || 'Profile Proposed'
+      ) */
+      return (
+        demand.Status === 'Profiles Required' ||
+        demand.Status === 'Demand Validation' ||
+        demand.Status === 'Profile Proposed'
+      )
+    })
+    return filteredDemands
+  }
+
+  const getRequiredSupplyStatus = (allSupply) => {
+    const filteredSupplys = allSupply.filter((supply) => {
+      return (
+        supply.ApplicantStatus === 'Screening' ||
+        supply.ApplicantStatus === 'L1 select' ||
+        supply.ApplicantStatus === 'L2 select' ||
+        supply.ApplicantStatus === 'Client select'
+      )
+    })
+    return filteredSupplys
+  }
+
+  const formatSkillForBarchart = (allSkills, filteredSupply, filteredDemand) => {
+    allSkills.forEach((skill, index) => {
+      const skillName = skill.SkillName
+      console.log(skillName, ' index ', index)
+      const supplies = filteredSupply.filter((supply) => supply.SkillName === skillName)
+      const demands = filteredDemand.filter((demand) => demand.SkillName === skillName)
+      console.log('Supplies for skill ', supplies)
+      console.log('Demands for skill ', demands)
+    })
+  }
+
+  useEffect(() => {
+    const filteredDemand = getRequiredDemandStatus(allDemand)
+    const filteredSupply = getRequiredSupplyStatus(allSupply)
+    console.log('Demand ', filteredDemand)
+    console.log('Supply ', filteredSupply)
+    console.log(allSkills)
+    formatSkillForBarchart(allSkills, filteredSupply, filteredDemand)
+  }, [allDemand, allSupply])
 
   return (
     <CG.Box width='48rem' boxSizing='border-box'>
