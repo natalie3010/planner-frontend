a9e68185f77810bc6936cdde24b33829371581e5
I Mohamad <ismail.mohamad@capgemini.com>
2022-08-25 10:31:09 +0100

    extracted the reusable functions into util and format files

--- src/Components/BarChart.jsx
+++ src/Components/BarChart.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { CG } from 'cap-shared-components'
 import { demandDataset, groupedOptions, supplyDataset } from '../Data/Data'
+import { getRequiredBarchartDemandStatus, getRequiredBarchartSupplyStatus } from '../Utils/util'
+import { formatDataForBarchart } from '../Data/Format'
 
 export const BarChart = ({ navigateToListPage, allDemand, allSupply, allSkills }) => {
   const [formattedStackedData, setFormattedStackedData] = useState(null)
@@ -13,61 +15,16 @@ export const BarChart = ({ navigateToListPage, allDemand, allSupply, allSkills }
     navigateToListPage(type, skillName)
   }
 
-  const getRequiredDemandStatus = (allDemand) => {
-    const filteredDemands = allDemand.filter((demand) => {
-      return (
-        demand.Status === 'Profiles Required' ||
-        demand.Status === 'Demand Validation' ||
-        demand.Status === 'Profile Proposed'
-      )
-    })
-    return filteredDemands
-  }
-
-  const getRequiredSupplyStatus = (allSupply) => {
-    const filteredSupplys = allSupply.filter((supply) => {
-      return (
-        supply.ApplicantStatus === 'Screening' ||
-        supply.ApplicantStatus === 'L1 select' ||
-        supply.ApplicantStatus === 'L2 select' ||
-        supply.ApplicantStatus === 'Client select'
-      )
-    })
-    return filteredSupplys
-  }
-
-  const formatSkillForBarchart = (allSkills, filteredSupply, filteredDemand) => {
-    const dashboardDataset = {
-      labels: [],
-      datasets: [],
-    }
-
-    allSkills.forEach((skill, skillIndex) => {
-      const skillName = skill.SkillName
-      dashboardDataset.labels.push(skillName)
-
-      const supplies = filteredSupply.filter((supply) => supply.SkillName === skillName)
-      const demands = filteredDemand.filter((demand) => demand.SkillName === skillName)
-
-      supplyDataset.forEach((obj) => {
-        const label = obj.label
-        const labelSupplyCount = supplies.filter((supply) => supply.ApplicantStatus === label).length
-        obj.data[skillIndex] = labelSupplyCount
-      })
-      demandDataset.forEach((obj) => {
-        const label = obj.label
-        const labelDemandCount = demands.filter((demand) => demand.Status === label).length
-        obj.data[skillIndex] = labelDemandCount
-      })
-    })
-    dashboardDataset.datasets = [...supplyDataset, ...demandDataset]
-    return dashboardDataset
-  }
-
   useEffect(() => {
-    const filteredDemand = getRequiredDemandStatus(allDemand)
-    const filteredSupply = getRequiredSupplyStatus(allSupply)
-    const formattedDataset = formatSkillForBarchart(allSkills, filteredSupply, filteredDemand)
+    const filteredDemand = getRequiredBarchartDemandStatus(allDemand)
+    const filteredSupply = getRequiredBarchartSupplyStatus(allSupply)
+    const formattedDataset = formatDataForBarchart(
+      allSkills,
+      filteredSupply,
+      filteredDemand,
+      supplyDataset,
+      demandDataset
+    )
     setFormattedStackedData(formattedDataset)
   }, [allDemand, allSupply, allSkills])
 
