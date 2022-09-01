ceb98d2151473d48e16c1e42df3545edbc728950
Faizah.Niniola <faizah.niniola@capgemini.com>
2022-09-01 16:59:51 +0100

    no white spaces

--- src/Validations/ListClientsValidation.js
+++ src/Validations/ListClientsValidation.js
@@ -4,6 +4,6 @@ import formValidators from '../../formValidatorsConfig.json'
 const clientInputs = formValidators.addClientForm.inputs
 
 export const clientSchema = yup.object().shape({
-  clientID: clientInputs.clientID.validators[0].required && yup.string().required(),
-  clientName: clientInputs.clientName.validators[0].required && yup.string().required(),
+  clientID: clientInputs.clientID.validators[0].required && yup.string().trim('no white space').required(),
+  clientName: clientInputs.clientName.validators[0].required && yup.string().trim('no white space').required(),
 })
