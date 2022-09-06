10c72b02aadd1bde75c5f4c0abfc1e86a178af34
sadhanach <sadhana.cheruku@capgemini.com>
2022-09-06 12:40:49 +0100

    changes to about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -133,6 +133,22 @@ export const About = () => {
             +44 7444999999
           </CG.Body>
         </div>
+        <div style={{ margin: '20px', padding: '20px', border: '1px solid #d4d4d4' }}>
+        <CG.Body size='XL' fontWeight='bold' style={{ marginBottom: '10px' }}>
+            Scheduled Maintainance of Workforce Planner
+          </CG.Body>
+          <CG.Body size='M'>
+          Please be advised that there will be scheduled downtime across our network from every Thursday-16.30PM GMT  to Thursday-17.00PM GMT  . 
+          This is because we are performing work on (functions/parts of the network).
+          <CG.Body size='M'>End users are requested not to use the workforce planner during this time.
+          </CG.Body>
+          
+          As a result, you may experience problems accessing the Services.
+          </CG.Body>
+          <CG.Body size='M'>
+          For more information, or if you have any questions, please Contact Customer Support.
+            </CG.Body>
+        </div>
         <div
           style={{
             margin: '20px',
