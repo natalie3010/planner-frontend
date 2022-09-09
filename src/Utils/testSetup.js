1c2dc1d5f9fee957c4738764091669fffcbb628a
Ankita Bhagade <ankita.bhagade@gmail.com>
2022-09-09 17:32:01 +0100

    updated store configuration and created testSetup util for container testing

---
+++ src/Utils/testSetup.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { Provider } from 'react-redux'
+import {setupStore} from '../store'
+
+export const renderWithProviders = (
+  container,
+  {
+    preloadedState = {},
+    store = setupStore(preloadedState),
+    ...renderOptions
+  } = {}
+) => {
+  const Wrapper = ({ children }) => <Provider store={store}>{children}</Provider>
+
+  return { store, ...render(container, { wrapper: Wrapper, ...renderOptions }) }
+}
