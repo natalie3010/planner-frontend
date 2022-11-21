c7b04c4e7b0e97fa7b83305c41c6ff617968d891
Jalila Yousuf <jalila.yousuf@capgemini.com>
2022-11-21 10:51:31 +0000

    removed footer from login screen

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,20 +1,32 @@
 import React from 'react'
 import { CG } from 'cap-shared-components'
+import { useLocation } from 'react-router-dom'
 import logo from './Assets/Capture.PNG'
 
-
 export const Footer = () => {
+  const location = useLocation()
   return (
-    <CG.Footer style={{ marginTop: 'auto', backgroundColor: 'white'}}>
-     
-      <CG.Box>
-        <a href='/workforcePlanner'>
-        <img src={logo} alt="logo" style={{width: '94px', height: '30px', display: 'flex', alignItems: 'center', width: '94px',  left: '74px', top: '767px'}} />
-        </a>
-        
-      </CG.Box>
-    
-    
-    </CG.Footer>
+    location.pathname !== '/account/login' &&
+    location.pathname !== '/' && (
+      <CG.Footer style={{ marginTop: 'auto', backgroundColor: 'white' }}>
+        <CG.Box>
+          <a href='/workforcePlanner'>
+            <img
+              src={logo}
+              alt='logo'
+              style={{
+                width: '94px',
+                height: '30px',
+                display: 'flex',
+                alignItems: 'center',
+                width: '94px',
+                left: '74px',
+                top: '767px',
+              }}
+            />
+          </a>
+        </CG.Box>
+      </CG.Footer>
+    )
   )
 }
